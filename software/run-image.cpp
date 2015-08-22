27c0e952d7203a8bb0df30a3f0baa416b17280a8
meg768 <magnus@egelberg.se>
2015-08-22 20:34:59 +0200

    -

--- software/run-image.cpp
+++ software/run-image.cpp
@@ -73,7 +73,7 @@ int main (int argc, char *argv[])
 
 		const Magick::PixelPacket *pixels = image.getConstPixels(0, 0, 32, 32);
 		
-		Magick::Image img("32x32", "black");
+		Magick::Image img(32, 32, "black");
 		img.composite(image, 0, 0, Magick::OverCompositeOp);
 		
 		if (rotate != 0)
