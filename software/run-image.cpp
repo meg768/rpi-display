d516763236db8edcbc954d38e956607476423dbf
meg768 <magnus@egelberg.se>
2015-08-23 00:07:19 +0200

    -

--- software/run-image.cpp
+++ software/run-image.cpp
@@ -88,7 +88,7 @@ int main (int argc, char *argv[])
 	if (mode == "scroll") {
 
 		if (true) {
-			Magick::Image img(Magick::Geometry(image.rows(), image.columns() + 2 * matrixWidth), "black");
+			Magick::Image img(Magick::Geometry(image.columns() + 2 * matrixWidth, image.rows(), "black");
 			img.composite(image, matrixWidth, 0, Magick::OverCompositeOp);
 			
 			image = img;
