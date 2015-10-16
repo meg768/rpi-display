4064a511091b945eed16487a92af80646121a40b
meg768 <magnus@egelberg.se>
2015-10-17 01:27:26 +0200

    -

--- raspberry/matrix/run-text.cpp
+++ raspberry/matrix/run-text.cpp
@@ -48,7 +48,7 @@ int main (int argc, char *argv[])
 			delay = 0;
 
 		double delayFactor = (32.0 * 32.0) / ((double)matrixWidht * (double)matrixHeight);
-		double fontFactor  = 1.0; //(double)matrix.height() / 32.0;
+		double fontFactor  = (double)matrixHeight / 32.0;
 		
 		fontSize = fontSize * fontFactor;
 		
