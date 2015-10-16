8421a7f2895c1fd9ff0b6df422db34e8dc2f1ebc
meg768 <magnus@egelberg.se>
2015-10-17 01:03:23 +0200

    -

--- raspberry/matrix/run-text.cpp
+++ raspberry/matrix/run-text.cpp
@@ -17,7 +17,10 @@ int main (int argc, char *argv[])
 	
 	try {
 		Matrix matrix;
-		
+
+		int matrixWidht  = matrix.width();
+		int matrixHeight = matrix.height();
+
 		while ((option = getopt(argc, argv, "t:i:s:c:f:x:")) != -1) {
 			switch (option) {
 				case 'x':
@@ -44,7 +47,7 @@ int main (int argc, char *argv[])
 		if (delay < 0)
 			delay = 0;
 
-		double delayFactor = (32.0 * 32.0) / ((double)matrix.width() * (double)matrix.height());
+		double delayFactor = (32.0 * 32.0) / ((double)matrixWidht * (double)matrixHeight);
 		double fontFactor  = 1.0; //(double)matrix.height() / 32.0;
 		
 		fontSize = fontSize * fontFactor;
@@ -53,7 +56,7 @@ int main (int argc, char *argv[])
 			iterations = 1;
 		
 		
-		Magick::Image tmp(Magick::Geometry(matrix.width(), matrix.height()), "blue");
+		Magick::Image tmp(Magick::Geometry(matrixWidht, matrixHeight), "blue");
 		
 		printf("delayFactor %f\n", delayFactor);
 		
@@ -68,32 +71,30 @@ int main (int argc, char *argv[])
 		Magick::TypeMetric metric;
 		tmp.fontTypeMetrics(text, &metric);
 		
-		Magick::Image image(Magick::Geometry(metric.textWidth() + matrix.width(), matrix.height()), "green");
+		Magick::Image image(Magick::Geometry(metric.textWidth() + matrixWidht, matrixHeight), "green");
 		image.font(fontFile);
 		image.strokeColor("transparent");
 		image.fillColor(textColor);
 		image.fontPointsize(fontSize);
 		
-		image.draw(Magick::DrawableText(matrix.width(), matrix.height() / 2 + metric.textHeight() / 2.0 + metric.descent(), text));
+		image.draw(Magick::DrawableText(matrixWidht, matrixHeight / 2 + metric.textHeight() / 2.0 + metric.descent(), text));
 
 		int imageWidth   = image.columns();
 		int imageHeight  = image.rows();
-		int matrixWidht  = matrix.width();
-		int matrixHeight = matrix.height();
 
 
 		const Magick::PixelPacket *pixels = image.getConstPixels(0, 0, imageWidth, imageHeight);
 
 		for (int count = 0; count < iterations; count++) {
 
-			for (int offsetX = 0, offsetY = 0; offsetX < imageWidth; offsetX++) {
+			for (int offsetX = 0, offsetY = 0; offsetX < imageWidth - matrixWidht; offsetX++) {
 				matrix.clear();
 
 				const Magick::PixelPacket *pp = pixels + offsetX;
 				
 				for (int y = 0; y < matrixHeight; y++) {
 					for (int x = 0; x < matrixWidht; x++) {
-						const Magick::PixelPacket *pp = pixels + x + y * imageWidth;
+						const Magick::PixelPacket *pp = pixels + x + (y * imageWidth);
 						uint8_t red   = pp->red;
 						uint8_t green = pp->green;
 						uint8_t blue  = pp->blue;
