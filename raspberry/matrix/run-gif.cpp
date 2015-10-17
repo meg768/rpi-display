44c7acb2899c94225a412c6848f6c9cacb98553e
meg768 <magnus@egelberg.se>
2015-10-17 23:07:57 +0200

    -

--- raspberry/matrix/run-gif.cpp
+++ raspberry/matrix/run-gif.cpp
@@ -1,4 +1,177 @@
 
+#include "ImageAnimation.h"
+
+
+class GifAnimation : public ImageAnimation {
+	
+public:
+	
+	GifAnimation(Matrix *matrix) : ImageAnimation(matrix) {
+		_iterations = -1;
+		_fileName   = "";
+	}
+	
+	
+	void fileName(const char *fileName) {
+		_fileName = fileName;
+	}
+	
+	virtual int run() {
+		
+		try {
+			
+			if (_fileName.length() == 0) {
+				
+				char folder[1000];
+				sprintf(folder, "./animations/%dx%d", _matrix->width(), _matrix->height());
+				
+				_fileName = folder;
+			}
+			
+			if (_fileName.length() != 0) {
+				
+				for (;;) {
+					struct stat status;
+					
+					if (stat(_fileName.c_str(), &status) != 0) {
+						fprintf(stderr, "Cannot open file.\n");
+						exit(-1);
+					}
+					
+					if (S_ISREG(status.st_mode))
+						break;
+					
+					else if (S_ISDIR(status.st_mode)) {
+						DIR *dir = opendir(_fileName.c_str());
+						
+						std::vector <string> files;
+						
+						if (dir != NULL) {
+							struct dirent *entry;
+							
+							while ((entry = readdir(dir)) != NULL) {
+								if (entry->d_name[0] != '.')
+									files.push_back(entry->d_name);
+							}
+							
+							closedir(dir);
+						}
+						
+						if (files.size() > 0)
+							_fileName = _fileName + "/" + files[rand() % files.size()];
+						else {
+							fprintf(stderr, "No files in directory.\n");
+							exit(-1);
+						}
+					}
+					else {
+						fprintf(stderr, "Funny file.\n");
+						exit(-1);
+					}
+				}
+				
+			}
+			
+			
+			if (_fileName.length() == 0) {
+				fprintf(stderr, "No animation specified.\n");
+				exit(-1);
+			}
+			
+			std::vector<Magick::Image> frames;
+			Magick::readImages(&frames, _fileName.c_str());
+			
+			std::vector<Magick::Image> images;
+			Magick::coalesceImages(&images, frames.begin(), frames.end());
+			
+			ImageAnimation::images(images);
+			ImageAnimation::run();
+			
+			
+		}
+		catch (std::exception &error) {
+			fprintf(stderr, "Could not start animation: %s\n", error.what());
+			return -1;
+		}
+		
+		return 0;
+	}
+	
+	
+private:
+	string _fileName;
+};
+
+
+int main (int argc, char *argv[])
+{
+	srand(time(NULL));
+	
+	Magick::InitializeMagick(*argv);
+	
+	Matrix matrix;
+	GifAnimation animation(&matrix);
+	
+	animation.duration(60);
+	animation.delay(10);
+	
+	int option = 0;
+	
+	while ((option = getopt(argc, argv, "d:x:i:f:")) != -1) {
+		switch (option) {
+			case 'd':
+				animation.duration(atoi(optarg));
+				break;
+			case 'x':
+				animation.delay(atof(optarg));
+				break;
+			case 'i':
+				animation.iterations(atoi(optarg));
+				break;
+			case 'f':
+				animation.fileName(optarg);
+				break;
+		}
+	}
+	
+	return animation.run();
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "matrix.h"
 #include "animation.h"
 
@@ -197,7 +370,7 @@ int main (int argc, char *argv[])
 }
 
 
-
+*/
 
 
 
