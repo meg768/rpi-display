29745394c93b81f4bc49f2deedd1b110960432a3
meg768 <magnus@egelberg.se>
2015-10-17 13:58:43 +0200

    -

--- raspberry/matrix/run-text.cpp
+++ raspberry/matrix/run-text.cpp
@@ -7,6 +7,129 @@ int main (int argc, char *argv[])
 	
 	const char *textColor  = "red";
 	const char *fontName   = "Arial-Bold";
+	
+	int option       = 0;
+	int iterations   = 1;
+	double fontSize  = 20.0;
+	double delay     = 18.0;
+	
+	std::string text = "ABC 123";
+	
+	try {
+		Matrix matrix;
+		
+		int matrixWidht  = matrix.width();
+		int matrixHeight = matrix.height();
+		
+		while ((option = getopt(argc, argv, "t:i:s:c:f:x:")) != -1) {
+			switch (option) {
+				case 'x':
+					delay = atof(optarg);
+					break;
+				case 'i':
+					iterations = atoi(optarg);
+					break;
+				case 's':
+					fontSize = atof(optarg);
+					break;
+				case 'c':
+					textColor = optarg;
+					break;
+				case 'f':
+					fontName = optarg;
+					break;
+				case 't':
+					text = optarg;
+					break;
+			}
+		}
+		
+		if (delay < 0)
+			delay = 0;
+		
+		double delayFactor = (32.0 * 32.0) / ((double)matrixWidht * (double)matrixHeight);
+		double fontFactor  = 1.0 + (((double)matrixHeight - 32.0) / 32.0) * 0.2;
+		
+		
+		fontSize = fontSize * fontFactor;
+		
+		if (iterations == 0)
+			iterations = 1;
+		
+		
+		Magick::Image tmp(Magick::Geometry(matrixWidht, matrixHeight), "black");
+		
+		printf("delayFactor %f\n", delayFactor);
+		
+		char fontFile[200];
+		sprintf(fontFile, "./fonts/%s.ttf", fontName);
+		
+		tmp.font(fontFile);
+		tmp.strokeColor(textColor);
+		tmp.fillColor(textColor);
+		tmp.fontPointsize(fontSize);
+		
+		Magick::TypeMetric metric;
+		tmp.fontTypeMetrics(text, &metric);
+		
+		Magick::Image image(Magick::Geometry(metric.textWidth() + 2 * matrixWidht, matrixHeight), "black");
+		image.font(fontFile);
+		image.strokeColor("transparent");
+		image.fillColor(textColor);
+		image.fontPointsize(fontSize);
+		image.draw(Magick::DrawableText(matrixWidht, matrixHeight / 2.0 + metric.textHeight() / 2.0 + metric.descent(), text));
+
+		int imageWidth   = image.columns();
+		int imageHeight  = image.rows();
+
+		std::vector<Magick::Image> frames;
+
+		if (true) {
+			
+			int count = imageWidth - matrixWidht;
+			
+			for (int i = 0; i < count; i++) {
+				Magick::Image frame(Magick::Geometry(matrixWidht, matrixHeight), "black");
+				frame.composite(image, i, 0, Magick::CopyCompositeOp);
+				
+				frames.push_back(frame);
+			}
+		}
+		
+		if (true) {
+			int count = frames.size();
+			
+			for (int i = 0; i < count; i++) {
+				Magick::Image &frame = frames[i];
+
+				matrix.drawImage(frame);
+				matrix.refresh();
+				usleep(int(delay * 1000.0 * delayFactor));
+
+			}
+		}
+		
+
+		
+	}
+	catch (std::exception &error) {
+		fprintf(stderr, "%s\n", error.what());
+		return -1;
+	}
+	
+	
+	return 0;
+}
+
+
+/*
+
+int main (int argc, char *argv[])
+{
+	Magick::InitializeMagick(*argv);
+	
+	const char *textColor  = "red";
+	const char *fontName   = "Arial-Bold";
 
 	int option       = 0;
 	int iterations   = 1;
@@ -118,5 +241,5 @@ int main (int argc, char *argv[])
 	
     return 0;
 }
-
+*/
 
