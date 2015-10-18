736fde65b2d60d9f1200f436111afd19340a5adb
meg768 <magnus@egelberg.se>
2015-10-18 18:54:37 +0200

    -

--- raspberry/matrix/run-gif.cpp
+++ raspberry/matrix/run-gif.cpp
@@ -91,7 +91,7 @@ public:
 				Magick::Image overlayImage(Magick::Geometry(_matrix->width(), _matrix->height()), "transparent");
 				overlayImage.font("./fonts/Arial-Bold.ttf");
 				overlayImage.strokeColor("transparent");
-				overlayImage.fillColor(Magick::Color(0, 0, 0, 128));
+				overlayImage.fillColor(Magick::Color(0, 0, 0, 64));
 				overlayImage.fontPointsize(20);
 
 				Magick::TypeMetric metric;
