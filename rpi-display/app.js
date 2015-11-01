366d933d2e8fdf16bfe83018528d566145cdb75d
meg768 <magnus@egelberg.se>
2015-11-01 02:01:32 +0100

    -

--- rpi-display/app.js
+++ rpi-display/app.js
@@ -116,7 +116,7 @@ function main() {
 		var rule = new schedule.RecurrenceRule();
 	
 		//rule.hour   = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-		rule.minute = new schedule.Range(0, 59, 1);
+		rule.minute = new schedule.Range(0, 59, 5);
 			
 		function hslToRgb(h, s, l){
 		    
