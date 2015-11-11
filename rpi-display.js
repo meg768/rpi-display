d59c2062a42291054fc4cf5ec3ff1b323e6993b3
meg768 <magnus@egelberg.se>
2015-11-11 19:15:16 +0100

    -

--- rpi-display.js
+++ rpi-display.js
@@ -1,342 +1,277 @@
 var schedule = require('node-schedule');
 var util     = require('util');
 var minimist = require('minimist');
+var numeral  = require('numeral');
+var moment   = require('moment');
 
+var config   = require('./scripts/config.js');
 var sprintf  = require('./scripts/sprintf.js');
 var random   = require('./scripts/random.js');
 var matrix   = require('./scripts/matrix.js');
 
 
-function main() {
+function App() {
 
-	var args = minimist(process.argv.slice(2));
+	var _this = this;
 
-	// Set the time zone according to config settings
-	process.env.TZ = 'Europe/Stockholm';
+	function fetchRates(tickers, callback) {
 
-	if (args.config == undefined) {
-		console.log('No configuration speciefied. Use the --config option.');
-		process.exit(-1);
-	}
-
-	// Make sure we configure the size of the display 
-	matrix.options.paths.animations = sprintf('./animations/%s', args.config);
-	matrix.options.paths.emojis = sprintf('./images/emojis/%s', args.config);
-
-	if (args.config == '32x32') {
-		matrix.options.config = '32x32';
-		matrix.options.defaults.image.delay = 30;
-		matrix.options.defaults.text.delay = 30;
-		matrix.options.defaults.text.size = 20;
-	}
-
-	if (args.config == '96x96') {
-		matrix.options.config = '96x96';
-		matrix.options.defaults.image.delay = 20;
-		matrix.options.defaults.text.delay = 20;
-		matrix.options.defaults.text.size = 24;
-	}
-
-	if (args.config == 'phi') {
-		matrix.options.config = '32x32';
-		matrix.options.defaults.image.delay = 30;
-		matrix.options.defaults.text.delay = 30;
-		matrix.options.defaults.text.size = 20;
-	}
-	
-	function enableRates() {
-	
-		var config = {
-			tickers: [
-				{ id:'USDSEK', tags: {name: 'USD/SEK'} },
-				{ id:'EURSEK', tags: {name: 'EUR/SEK'} },
-				{ id:'EURUSD', tags: {name: 'UER/USD'} },
-				
-			],
-			
-			schedule: {
-				hour:   new schedule.Range(7, 23),
-				minute: new schedule.Range(8, 59, 20)
-			}
-			
-		};
 		var Rates = require('./scripts/xchange.js');
-		var rates = new Rates(config);
+		var rates = new Rates(tickers);
 	
-		rates.on('xchange', function(xchange) {
-			console.log(xchange);
-
-			var display = new matrix.Display();
-			var options = {};
-			
-			options.color = 'rgb(0,0,255)';
-			
-			xchange.forEach(function(rate) {
-				display.text(sprintf('%s   %.2f', rate.name, rate.value));
-			});
-
-			display.send();
+		rates.fetch(function(rates) {
+			console.log('XCHANGE', rates);
+			callback(rates);
 		});
-	
-	
-	
 	}	
+
 	
-	
-	function enableRSS() {
-	
-		var config = {
-			feeds: [
-				{
-					url: 'http://www.di.se/rss', 
-					tags: { name: 'Di' },
-					
-					schedule: {
-						hour:   new schedule.Range(7, 23),
-						minute: new schedule.Range(3, 59, 20)
-					}
-				},
-				{
-					url: 'http://www.svd.se/?service=rss&type=senastenytt',
-					tags: {name: 'SvD' },
-	
-					schedule: {
-						hour:   new schedule.Range(7, 23),
-						minute: new schedule.Range(6, 59, 20)
-					}
-				},
-				{
-					url: 'http://news.google.com/news?pz=1&cf=all&ned=sv_se&hl=sv&topic=h&num=3&output=rss',
-					tags: {name: 'Google' },
-	
-					schedule: {
-						hour:   new schedule.Range(7, 23),
-						minute: new schedule.Range(9, 59, 20)
-					}
-				},
-				{
-					url: 'http://www.sydsvenskan.se/rss.xml',
-					tags: {name: 'Sydsvenskan' },
-	
-					schedule: {
-						minute: new schedule.Range(12, 59, 20)
-					}
-				}
-			]
-		};
+	function displayRates(rates) {
+
+		var display = new matrix.Display();
+		var options = {};
+		
+		options.color = 'rgb(0,0,255)';
+		
+		rates.forEach(function(rate) {
+			display.text(sprintf('%s   %.2f', rate.name, rate.value));
+		});
+
+		display.send();
+		
+	}
+
+
+	function fetchRSS(feeds, callback) {
 		
 		var RSS = require('./scripts/rss.js');
-		var rss = new RSS(config);
+		var rss = new RSS(feeds);
 	
-		rss.on('rss', function(rss) {
-			console.log('RSS:', rss);
-			
-			var display = new matrix.Display();
-			var options = {};
-			
-			options.color = 'rgb(0,0,255)';
-			
-			display.text(sprintf('Nyheter från %s', rss.name));
-			
-			rss.messages.forEach(function(message) {
-				var text = '';
-				
-				if (message.category != undefined)
-					text += message.category;
-					
-				if (text != '')
-					text += ' - ';
-					
-				if (message.title != undefined)
-					text += message.title; 
-					
-				display.text(text, options);
-						
-			});
-			
-			display.send();
-
-
+		rss.fetch(function(messages) {
+			console.log('RSS:', messages);
+			callback(messages);
 		});
+
+	}
 	
 	
-	}		
+	function displayRSS(messages) {
 
-
-	
-	function enableQuotes() {
-		var numeral = require('numeral');
-
-		var config = {
-			schedule: {
-				hour   : new schedule.Range(7, 23),
-				minute : new schedule.Range(13, 59, 13)
-			},
-			
-			tickers : [
-				
-				{ id: '^OMX',    tags: {name: 'OMX'}},
-				{ id: '^GSPC',   tags: {name: 'S&P 500'}},
-				{ id: '^IXIC',   tags: {name: 'NASDAQ'}},
-				{ id: 'PHI.ST',  tags: {name: 'PHI', currency: 'SEK'}},
-				{ id: 'HM-B.ST', tags: {name: 'H&M', currency: 'SEK'}}
-				
-			]
-		};
+		var display = new matrix.Display();
+		var options = {};
 		
-		var Quotes = require('./scripts/quotes.js');
-		var quotes = new Quotes(config);
+		options.color = 'rgb(0,0,255)';
+		
+		display.text(sprintf('Nyheter från %s', messages[0].name));
+		
+		messages.forEach(function(message) {
+			var text = '';
+			
+			if (message.category != undefined)
+				text += message.category;
+				
+			if (text != '')
+				text += ' - ';
+				
+			if (message.title != undefined)
+				text += message.title; 
+				
+			display.text(text, options);
+					
+		});
+		
+		display.send();
+		
+	}
+		
+		
+	function fetchQuotes(tickers, callback) {
 	
-		quotes.on('quotes', function(data) {
+		var Quotes = require('./scripts/quotes.js');
+		var quotes = new Quotes(tickers);
+
+		quotes.fetch(function(quotes) {
+			console.log('QUOTES:', quotes);
+			callback(quotes);
+		});
+	}
+	
+	function displayQuotes(quotes) {
+		
+		if (quotes.length > 0) {
 			var display = new matrix.Display();
-			var now = new Date();
-			var space = '   ';
 			
-			var options = {};
-			
-			data.forEach(function(quote) {
+			quotes.forEach(function(quote) { 
+				var options = {};
+				options.color = 'rgb(255, 255, 255';
 
-
+				display.image(quote.logo, {scroll:'horizontal'});
+				display.text(numeral(quote.price).format('0,000.00') + ' SEK', options);
+				
 				if (quote.change == 0)
 					options.color = 'rgb(0,0,255)';
 				if (quote.change < 0)
 					options.color = 'rgb(255,0,0)';
 				if (quote.change > 0)
 					options.color = 'rgb(0,255,0)';
+		
+				display.text(numeral(quote.change).format('+0.0') + '%%', options);
+
+				options.color = 'rgb(0,0,255)';
+				display.text(sprintf('%d', Math.round(quote.volume / 100) * 100), options);
 				
-
-				var text = '';
-
-				text += quote.name + space;
-				text += numeral(quote.change).format('+0.0') + '%' + space;
-
-				if (quote.currency != undefined) {
-					text += numeral(quote.price).format('0,000.00') + space;
-				}
-
-	
-				display.text(text, options);
-	
 			});
-				
+
 			display.send();
 			
-			
-		});
+		}
 		
 	}
 	
-	function enableWeather() {
 	
-	
-		var config = {
-			woeid: '12883682',
-			
-			schedule: {
-				hour   : new schedule.Range(7, 23),
-				minute : [10, 40]
-			}
-		};	
-	
-		var Weather = require('./scripts/weather.js');
-		var weather = new Weather(config);
+	function displayAnimation() {
+		var display = new matrix.Display();
 
-		weather.on('forecast', function(forecast) {
-			console.log(forecast);
-			
-			var display = new matrix.Display();
-			
-			forecast.forEach(function(day) {
+		var pong = {};
+		pong.file = sprintf('./animations/96x96');
+		pong.duration = 60;
+		pong.iterations = 1000; // Bug! Had to set this, fix it...
 
-				display.text(sprintf('%s - %s, %d°', day.day, day.condition.toLowerCase(), Math.round(day.high)), {delay:15});
-				display.image(sprintf('./images/weather/%s/%s.png', args.config, day.image), {delay:15, scroll:'horizontal'});
+		var pacman = {};
+		pacman.file = sprintf('./animations/32x32/pacman.gif');
+		pacman.duration = 60;
+		pacman.iterations = 1000;
 
-			});
-			
-			display.send();
-			
-		});
+		display.animation(random.choose([pong, pacman]));
+		display.send();
+	}
 	
-	
-	
-	}	
-	
+
+	function displayClock() {
+		var display = new matrix.Display();
+		var now = new Date();
+
+		var options = {};
+		options.color = 'rgb(255, 0, 0)';
+
+		display.text(sprintf('%02d:%02d', now.getHours(), now.getMinutes()), options);	
+		display.send();	
 		
-	function enableClock() {
-		var colors = require('./scripts/colors.js');
-		var rule = new schedule.RecurrenceRule();
+	}
 	
-		//rule.hour   = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
-		rule.minute = new schedule.Range(0, 59, 5);
+
+	function displayFika() {
+		
+		var display = new matrix.Display();
+
+		display.emoji(48, {scroll:'horizontal', delay:30});
+		display.emoji(268, {scroll:'horizontal', delay:30});
 			
+		display.send();
+		
+	}
+	
+	function displayIdle() {
+		var now = new Date();
+		var display = new matrix.Display();
+
+		var options = {};
+		options.file = sprintf('./animations/96x96');
+		options.duration = -1;
+
+		display.animation(options);
+		display.send();	
+	}
+	
+	
+	function scheduleRecurrences() {
+		var rule = new schedule.RecurrenceRule();
+		var index = 0;
+		
+		rule.hour = new schedule.Range(7, 21, 1);
+		rule.minute = [15, 45];
 		
 		schedule.scheduleJob(rule, function() {
-			var display = new matrix.Display();
-			var now = new Date();
-			var hue = ((now.getHours() % 12) * 60 + now.getMinutes()) / 2;			
-			var color = colors.hslToRgb(hue / 360, 1, 0.5);
-			
-			var options = {};
-			options.color = sprintf('rgb(%d,%d,%d)', color.red, color.green, color.blue);
 
-			display.text(sprintf('%02d:%02d', now.getHours(), now.getMinutes()), options);	
-			display.send();	
+			switch(index % 4) {
+				case 0: {
+					fetchRSS({url: 'http://www.di.se/rss', name:'Dagens Industri'}, displayRSS);
+					break;
+				}
+				case 1: {
+					fetchRSS({url: 'http://www.sydsvenskan.se/rss.xml', name:'Sydsvenskan'}, displayRSS);
+					break;
+				}
+				case 2: {
+					fetchRSS({url: 'http://www.svd.se/?service=rss&type=senastenytt', name:'SvD'}, displayRSS);
+					break;
+				}
+				case 3: {
+					fetchRSS({url: 'http://news.google.com/news?pz=1&cf=all&ned=sv_se&hl=sv&topic=h&num=3&output=rss', name:'Google'}, displayRSS);
+					break;
+				}
+			}
+			
+			index = (index + 1) % 1000;
+		});
+	}
+
+		
+	function scheduleClock() {
+		var rule = new schedule.RecurrenceRule();	
+		rule.minute = new schedule.Range(0, 59, 5);
+		
+		schedule.scheduleJob(rule, function() {
+			displayClock();
 		});
 	}
 	
-
 	
-
+	function scheduleQuotes() {
 	
-	 
-	function sayHello() {
 	
-		var display = new matrix.Display();
-		
-		function getIP(device) {
-			var os = require('os');
-			var ifaces = os.networkInterfaces();
-		
-			var iface = ifaces[device];
+		var rule = new schedule.RecurrenceRule();
+		rule.hour = new schedule.Range(8, 18, 1);
+		rule.minute = new schedule.Range(0, 59, 1);
 			
-			if (iface != undefined) {
-			
-				for (var i in iface) {
-					var item = iface[i];
-			
-					if (item.family == 'IPv4')
-						return item.address;
-				}
-			}
+		schedule.scheduleJob(rule, function() {
+			fetchMyQuotes();
+		});
 		
-			return '';
-		}
+		// Get quote now
+		fetchMyQuotes();
 		
-		var wlan0 = getIP('wlan0');
-		var eth0 = getIP('eth0');
-
-		if (wlan0 != '') {
-			display.text(wlan0, {color:'blue'});
-		}
-			
-		if (eth0 != '') {
-			display.text(eth0, {color:'blue'});
-		}
-		
-		if (wlan0 != '' || eth0 != '')
-			display.emoji(435, {scroll:'horizontal'});
-
-		display.send();		
 	}
-	  
 
-	sayHello();
-	enableRates();
-	enableClock();
-	enableQuotes();
-	enableRSS();
-	enableWeather();
+	_this.run = function() {
+		// Set the time zone according to config settings
+		process.env.TZ = config.timezone;
+	
+		// Use swedish settings
+		moment.locale(config.locale);
 
+		config.matrix.width = 96;
+		config.matrix.height = 96;
+		config.matrix.defaults.text.font = 'Arial-Bold';
+		config.matrix.defaults.text.delay = 20;
+		config.matrix.defaults.text.size = 24;
+		config.matrix.defaults.image.delay = 20;
+
+		matrix.init();
+
+		matrix.idle(function() {
+			displayIdle();
+		});
+		
+		scheduleQuotes();
+		scheduleRecurrences();
+		
+	}
+	
 }
 
-main();
+
+var app = new App();
+app.run();
+
+
