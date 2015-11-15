dccedba21d280f2a45767a1d3c6f622424ee9a12
meg768 <magnus@egelberg.se>
2015-11-15 01:02:50 +0100

    -

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ def run():
 
 	matrix = RGBMatrix(32, 3, 3)
 	canvas = matrix.CreateFrameCanvas()
-	template = Image.open("./images/clock/clock1.png")
+	template = Image.open("./images/clock/swiss.png")
 	
 	while True:
 		image = renderClockImage(template);
