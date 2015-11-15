d52e0e429bcd506f37f3e6f899468a0a9ceafecd
meg768 <magnus@egelberg.se>
2015-11-15 01:46:21 +0100

    -

--- test.py
+++ test.py
@@ -84,14 +84,7 @@ def run():
 	
 	while True:
 		image = renderClockImage(template);
-
-		for y in range(0, 96):
-			for x in range(0, 96):
-				rgb = image.getpixel((x, y))
-				canvas.SetPixel(x, y, rgb[0], rgb[1], rgb[2])
-
-		canvas = matrix.SwapOnVSync(canvas)
-		#time.sleep(0.01)
+		renderImageOnCanvas(image, canvas)
 		
 
 def test():
@@ -101,44 +94,6 @@ def test():
 	image.save("clock.png");	
 
 
-
-def main(argv):
-	matrixWidth  = 96
-	matrixHeight = 96
-	fileName	  = "adafruit.png"
-	repeats	  = 1
-	delay		  = 22
-	pwm		  = 8
-	hold		  = 0
-	
-	#logging.basicConfig(filename='example.log',level=logging.DEBUG)
-
-	logger = logging.getLogger(__name__)	
-	
-	
-	time = datetime.datetime.now()
-	matrixImage = Image.new("RGBA", (matrixWidth, matrixHeight)) 
-
-	clock = Image.open("./images/clock/clock1.png")
-
-	backgroundImage = clock.crop((matrixWidth * 0, 0, matrixWidth * 0 + matrixWidth, matrixHeight))
-	foregroundImage = clock.crop((matrixWidth * 1, 0, matrixWidth * 1 + matrixWidth, matrixHeight))
-	hoursImage		 = clock.crop((matrixWidth * 2, 0, matrixWidth * 2 + matrixWidth, matrixHeight))
-	minutesImage	 = clock.crop((matrixWidth * 3, 0, matrixWidth * 3 + matrixWidth, matrixHeight))
-	secondsImage	 = clock.crop((matrixWidth * 4, 0, matrixWidth * 4 + matrixWidth, matrixHeight))
-	
-
-	secondsImage = secondsImage.rotate(-360.0 * (time.second / 60.0), Image.BILINEAR)
-	hoursImage = hoursImage.rotate(-360.0 * ((time.hour % 12) * 60 + time.minute) / (12.0 * 60.0), Image.BILINEAR)
-	minutesImage = minutesImage.rotate(-360.0 * (time.minute / 60.0), Image.BILINEAR)
-
-	matrixImage.paste(backgroundImage, [0, 0, 96, 96], backgroundImage)
-	matrixImage.paste(hoursImage, [0, 0, 96, 96], hoursImage)
-	matrixImage.paste(minutesImage, [0, 0, 96, 96], minutesImage)
-	matrixImage.paste(secondsImage, [0, 0, 96, 96], secondsImage)
-	matrixImage.paste(foregroundImage, [0, 0, 96, 96], foregroundImage)
-	matrixImage.save("./test.png")
-	
 
 	 
 
