b7e649d5d948cfeeaa8beaf6adc9a978863dc7b3
meg768 <magnus@egelberg.se>
2015-11-16 10:28:12 +0100

    -

--- matrix/run-clock.cpp
+++ matrix/run-clock.cpp
@@ -21,7 +21,8 @@ public:
 
 	void createImage(Magick::Image &image, int offset) {
 	
-		Magick::Image img(Magick::Geometry(96, 96), "red");
+		Magick::Image img(Magick::Geometry(96, 96), "transparent");
+		img.magick("RGBA");
 		
 		const Magick::PixelPacket *src = _image.getConstPixels(96 * offset, 0, 96, 96);
 		Magick::PixelPacket *dst = img.setPixels(0, 0, 96, 96);
