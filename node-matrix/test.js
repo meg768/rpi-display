2bf05d11b76f4d241c700250f7ee50693cfa72f7
meg768 <magnus@egelberg.se>
2016-05-11 22:05:40 +0200

    -

--- node-matrix/test.js
+++ node-matrix/test.js
@@ -4,8 +4,8 @@ console.log(addon.pow(4, 2));
 
 function draw() {
 
-	for (var x = 0; x < 20; x++)
-		for (var y = 0; y < 20; y++)
+	for (var x = 0; x < 32; x++)
+		for (var y = 0; y < 32; y++)
 			addon.setPixel(x, y, 255, 255, 255);
 	
 	addon.refresh();
